9abe302dd236278081215c9ded13490f40a774e0
trevor dewindt <trevor.dewindt@gmail.com>
2023-12-19 21:32:16 -0700

    add final code

--- server/app.py
+++ server/app.py
@@ -17,11 +17,37 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [p.to_dict() for p in Plant.query.all()]
+        return make_response(
+            plants,
+            200
+        )
+    
+    def post(self):
+        plant = Plant(
+            name = request.get_json()['name'],
+            image = request.get_json()['image'],
+            price = request.get_json()['price']
+        )
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(
+            plant.to_dict(),
+            201
+        )
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first().to_dict()
+        return make_response(
+            plant,
+            200
+        )
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"{self.name}, {self.image}, {self.price}"
